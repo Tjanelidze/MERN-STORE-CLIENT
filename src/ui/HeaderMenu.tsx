f1efea06d942badc20132dda27144bcc7b9dcf8f
Tedo <tedojanelidze7@gmail.com>
2024-07-26 21:40:17 +0400

    Add Hover effect on list items in header

--- src/ui/HeaderMenu.tsx
+++ src/ui/HeaderMenu.tsx
@@ -18,5 +18,21 @@ const StyledHeaderMenu = styled.ul`
   & > li {
     cursor: pointer;
     font-weight: 600;
+    position: relative;
+  }
+
+  & > li::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    height: 0.2rem;
+    width: 0;
+    background-color: #22223b;
+    transition: width 0.4s;
+  }
+
+  & > li:hover::after {
+    width: 100%;
   }
 `;
